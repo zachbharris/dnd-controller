b5fe1f9dfcb84676ccfc0a7ed3c0d0fd3c75d854
Zach B. Harris <zachbharris94@gmail.com>
2018-12-23 14:17:44 -0600

    changes are happening

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,50 +1,5 @@
-import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
-import { compose } from 'redux';
-import { connect } from 'react-redux';
-import { firestoreConnect } from 'react-redux-firebase';
-import { Card, Container, Grid } from 'semantic-ui-react';
+import React from 'react';
 
-class Dashboard extends Component {
-  render() {
-    const { auth, campaigns } = this.props;
+const Dashboard = () => <h1>Hello</h1>
 
-    if (!auth.uid) return <Redirect to="/login" />
-    return (
-      <Container>
-        <Grid stackable>
-          <Grid.Column width={6}>
-            <Card fluid>
-              <Card.Content>
-                <Card.Header content="Characters" />
-              </Card.Content>
-              <Card.Content>
-                poo
-              </Card.Content>
-              <Card.Content>
-                <Card.Description>
-                  more poo
-                </Card.Description>
-              </Card.Content>
-            </Card>
-          </Grid.Column>
-        </Grid>
-      </Container>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    auth: state.firebase.auth,
-    campaigns: state.firestore.ordered.campaigns,
-    characters: state.firestore.characters
-  }
-};
-
-export default compose(
-  connect(mapStateToProps),
-  firestoreConnect([
-    { collection: 'campaigns' }
-  ]),
-)(Dashboard);
+export default Dashboard;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,26 +2,10 @@ import React from "react";
 import { Route, Switch, withRouter } from "react-router-dom";
 
 import Dashboard from "../pages/Dashboard";
-import Campaign from "../pages/Campaign";
-import Profile from "../pages/Users/profile";
-
-import CampaignDetails from "../components/Campaigns/CampaignDetails";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Dashboard} />
-
-    {/* user routes */}
-    <Route path="/login" component={Login} />
-    <Route path="/register" component={Register} />
-    <Route path="/user/:id" component={Profile} />
-
-    {/* campaign routes */}
-    <Route path="/campaign/:type/:id" component={CampaignDetails} />
-    <Route path="/campaign/:type" component={Campaign} />
-    <Route path="/campaign/" component={Campaign} />
   </Switch>
 );
 
