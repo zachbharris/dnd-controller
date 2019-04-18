3b650cc9b010a338bf6f761e6ddaf10f98e6ba6d
Zach Harris <zachbharris94@gmail.com>
2019-04-17 23:02:25 -0500

    temp landing page while project is worked on

--- src/routes/Landing/index.jsx
+++ src/routes/Landing/index.jsx
@@ -1,11 +1,51 @@
 import React from "react";
+import styled from "styled-components";
+import { animated, useSpring, config } from "react-spring";
+
+import pattern from "../../dnd-pattern.svg";
 
 const LandingPage = () => {
+  const backgroundProps = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    config: config.slow
+  });
+
+  const logoProps = useSpring({
+    from: {
+      opacity: 0,
+      transform: `translateX(-1000%) rotate(-720deg)`
+    },
+    to: {
+      opacity: 1,
+      transform: `translateX(0) rotate(0deg)`
+    },
+    delay: 250,
+    config: {
+      mass: 3,
+      friction: 20,
+      tension: 75
+    }
+  });
+
   return (
-    <>
-      <p>Your face is doodoo</p>
-    </>
+    <Wrapper style={backgroundProps}>
+      <Logo style={logoProps} className="fal fa-dice-d20" />
+    </Wrapper>
   );
 };
 
 export default LandingPage;
+
+const Wrapper = styled(animated.div)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-image: url(${pattern});
+  min-height: 100vh;
+`;
+
+const Logo = styled(animated.i)`
+  font-size: 90px;
+`;
